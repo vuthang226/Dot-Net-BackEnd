87106952b12eb4f5104635c71978e3858d65fb8a
vuthang226 <vuthang226@gmail.com>
2021-07-04 22:48:46 +0700

    push code

---
+++ MISA1/MISA.ESHOP.Core/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.ESHOP.Core
+{
+    /// <summary>
+    /// Hang so
+    /// </summary>
+  public  class Constants
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string HeaderCompanyCode = "CompanyCode";
+    }
+}
---
+++ MISA1/MISA.ESHOP.Model/Dictionary/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.ESHOP.Model
+{
+    public  class InventoryItem
+    {
+        private string _name;
+        public string Name { get; set; }
+
+        public bool Validate(string shortName, decimal price)
+        {
+            return true;
+        }
+    }
+}
